79c1f610df9a503f4b24f82a7d9d96220467a3ff
alexholehouse <alex.holehouse@gmail.com>
2021-01-04 08:55:12 -0600

    Updated read_fasta to correctly deal with the protfasta keyword return_list

--- sparrow/tools/io.py
+++ sparrow/tools/io.py
@@ -1,5 +1,5 @@
 import protfasta
-from sparrow import protein
+from sparrow.protein import Protein
 
 def read_fasta(filename, **kwargs):
     """
@@ -17,15 +17,33 @@ def read_fasta(filename, **kwargs):
     **kwargs
         Keyword arguments as defined in protfasta.read_fasta
 
+    Returns
+    ------------
+    dict or list
+        Returns either a dictionary where keys are FASTA headers and values are sparrow.protein.Protein
+        objects OR a list of sparrow.protein.Protein objects (if the return_true keyword was set to True)
+
+
     """
 
     # read in file
     F = protfasta.read_fasta(filename, **kwargs)
 
+    if 'return_list' in kwargs:
+        return_list = kwargs['return_list']
+    else:
+        return_list = False
+
     # build a return dictionary of Protein objects
-    return_dict = {}
-    for i in F:
-        return_dict[i] = protein.Protein(F[i])
+    if return_list:
+        return_dict = []
+        for i in F:
+            return_dict.append(Protein(i[1]))
+
+    else:
+        return_dict = {}
+        for i in F:
+            return_dict[i] = Protein(F[i])
 
     return return_dict
     
