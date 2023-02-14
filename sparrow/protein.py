67a3f6160a866dcd4afb97c69b1394a4a8cdfa59
GGinell <GarrettGinell@gmail.com>
2023-02-14 13:19:56 -0600

    add feature to generate_phosphoisoforms

--- sparrow/protein.py
+++ sparrow/protein.py
@@ -5,6 +5,7 @@ from . import calculate_parameters
 from .visualize import sequence_visuals
 from sparrow import data
 from .sequence_analysis import sequence_complexity
+from .sequence_analysis import phospho_isoforms
 from .sequence_analysis import physical_properties
 from .sequence_analysis import elm
 
@@ -716,8 +717,44 @@ class Protein:
 
         return iwd.calculate_average_bivariate_inverse_distance_charge(linear_NCPR, self.sequence)
 
+    ## .................................................................
+    ##
+    def generate_phosphoisoforms(self, mode=['all', 'predict', 'custom'], phospho_rate=1, phosphosites=None):
+        """
+        Calls sequence_analysis.phospho_isoforms module and to get a list
+        of possible phosphoisoforms sequences. See module header for more documentation.
 
+        Phosphosites are then returned 'E' in amino acid sequence enabling aproximate calculation 
+        charge based sequence features with the presence of a phosphosite.
 
+        Parameters
+        ------------
+        sequence : str
+            Valid amino acid sequence
+
+        mode : str
+            Defition for how the phosphosites should be determined 
+            all = calls _get_all_phosphosites
+            predict = calls _predict_all_phosphosites
+            custom = uses inputed 'phosphosites' parameter as phosphosites 
+                     To use custom phosphosites must be defined
+        
+        phospho_rate : float 
+            Value between 0 and 1 which defines the maximum percent of phosphosites which
+            can be 'phosphorylated' in each sequence. Defult is 1 (IE all sites can be 
+            phosphorylated)
+
+        phosphosites : list
+            custom inputed list of valid phosphosite positions
+        
+        Returns
+        ------------
+        list
+            list of sequences for all posible phosphoisoforms where phosphosites amino acids are
+            replaced with 'E'. 
+        """
+        return phospho_isoforms.get_phosphoisoforms(self.sequence, mode=mode, phospho_rate=phospho_rate, 
+            phosphosites=phosphosites)
         
     # .................................................................
     #            
---
+++ sparrow/sequence_analysis/phospho_isoforms.py
@@ -0,0 +1,204 @@
+"""
+Snippit to build and adapted from localcider to get all phosphoisoforms 
+of an amino acid sequence which is to integrated into sparrow
+
+By : Garrett M. Ginell
+2023-02-08
+
+BASIC workflow is as followed:
+
+  To get a list of run get_phosphoisoforms"
+
+    phosphoSeqome = get_phosphoisoforms(sequence, mode='predict')
+    # for options see various run variations in function header
+    
+  Then once you get the phosphoisoforms from the list above you can iterate
+  the list and calculate a sequence parameter of choice and build a distribution: 
+
+    parameter_list = []
+    for s in phosphoSeqome:
+        parameter_list.append(Protein(s).my_parameter_of_choice)
+  
+  This distribution can then be compared back to value of the original sequence:
+
+    Protein(sequence).my_parameter_of_choice 
+"""
+
+from sparrow.predictors.phosphorylation import ser_phosphorylation_predictor
+from sparrow.predictors.phosphorylation import thr_phosphorylation_predictor
+from sparrow.predictors.phosphorylation import tyr_phosphorylation_predictor
+import itertools
+
+
+## -----------------------------------------
+##
+def _predict_all_phosphosites(sequence):
+    """
+    Gets list of predicted phosphosites 
+
+    BASED OFF OF:
+        predictors in sparrow:
+        https://github.com/idptools/sparrow/tree/main/sparrow/predictors/phosphorylation
+
+    Parameters
+    ------------
+    seq : str
+        Valid amino acid sequence
+
+    Returns:
+    ----------
+    list
+        list of predicted positions of sites of phosphorylated T, S, and Y 
+        Note positions are returned as indexed from 0
+
+    """
+    pS = ser_phosphorylation_predictor.SerPhosphorylationPredictor().predict_ser_phosphorylation(sequence, return_sites_only=True)
+    pT = thr_phosphorylation_predictor.ThrPhosphorylationPredictor().predict_thr_phosphorylation(sequence, return_sites_only=True)
+    pY = tyr_phosphorylation_predictor.TyrPhosphorylationPredictor().predict_tyr_phosphorylation(sequence, return_sites_only=True)
+
+    return list(pS + pT + pY)
+
+## ----------------------------------------
+##
+def _get_all_phosphosites(sequence):
+    """
+
+    BASED OFF OF:
+
+     localcider.sequenceParameters.SequenceParameters().get_all_phosphorylatable_sites 
+
+    Function which returns a list of all the positions which *could* be
+    phosphorylated (i.e. are T/S/Y). NOTE this does not use any kind of
+    smart lookup, metadata, or analysis. It's literally, where are the Y/T/S
+    residues.
+    Note positions are returned as indexed from 0 
+    
+    Parameters
+    ------------
+    sequence : str
+        Valid amino acid sequence
+
+    Returns:
+    ----------
+    list
+        list of integers corresponding to S/T/Y positions in your sequence
+
+    """ 
+    sites = []
+    idx = 1
+    for i in sequence:
+        if i in ["Y", "S", "T"]:
+            sites.append(idx)
+        idx = idx 
+    return sites
+
+## -----------------------------------
+##
+def _build_phosphoSeqome(sequence, phosphosites, phospho_rate=1):
+    """
+    Build all phospho-isoforms based on provided phosphosites
+
+    Parameters
+    ------------
+    sequence : str
+        Valid amino acid sequence
+
+    phosphosites : list
+        List of valid phosphosite positions
+
+    phospho_rate : float 
+        Value between 0 and 1 which defines the maximum percent of phosphosites 
+        can be 'phosphorylated' a each sequence. Defult is 1 (IE all sites can be 
+        phosphorylated)
+
+    Returns
+    ----------
+    list
+        list of sequences for all posible phospho-isoforms 
+        based off of the provided inputed list of phosphosites
+
+        When phospho_rate = 1 (100%) 
+            the length of output list = 2^n where n=len(phosphosites)
+    """
+
+    _max_phospho_number = int(len(phosphosites)*phospho_rate)
+    ## GET ALL phospho-sequence combinations 
+    phosphoSeqome = []
+    phosphoSeqome_info = []
+    for phosphostatus in itertools.product("01", repeat=len(phosphosites)):
+        
+        if phosphostatus.count('1') > _max_phospho_number:
+            continue
+        newseq = list(sequence)
+
+        count = 0
+        indx = 0
+        # look over each element in our phosphosite on/off list
+        for i in phosphostatus:
+            # if that element is ON
+            if int(i) == 1:
+                # set the AA at that position to a negative residue (we use E but
+                # could be D)
+                newseq[phosphosites[indx]] = "E"
+                count+=1 
+            indx = indx + 1
+            
+        # now we've replaced some number of T/Y/S with E representing a different
+        # phosphostate
+        newseq = "".join(newseq)
+        phosphoSeqome.append(newseq)
+
+    return phosphoSeqome
+
+## -----------------------------------
+##
+def get_phosphoisoforms(sequence, mode=['all', 'predict', 'custom'], phospho_rate=1, 
+    phosphosites=None):
+    """
+    build all phospho-isoforms based on provided phosphosites
+
+    Parameters
+    ------------
+    sequence : str
+        Valid amino acid sequence
+
+    mode : str
+        Defition for how the phosphosites should be determined 
+
+        all = calls _get_all_phosphosites
+
+        predict = calls _predict_all_phosphosites
+
+        custom = uses inputed 'phosphosites' parameter as phosphosites 
+                 To use custom phosphosites must be defined
+    
+    phospho_rate : float 
+        Value between 0 and 1 which defines the maximum percent of phosphosites 
+        can be 'phosphorylated' a each sequence. Defult is 1 (IE all sites can be 
+        phosphorylated)
+
+    phosphosites : list
+        custom inputed list of valid phosphosite positions
+    
+    Returns
+    ------------
+    list
+        list of sequences for all posible phospho-isoforms 
+        based off of the list of phosphosites 
+    """
+
+    # get phosphosite positions
+    if mode is 'all':
+        _phosphosites = _get_all_phosphosites(sequence)
+    elif mode is 'predict':
+        _phosphosites = _predict_all_phosphosites(sequence)
+    elif mode is 'custom':
+        if phosphosites != None:
+            _phosphosites = phosphosites
+        else:
+            raise Exception('To use custom phosphosites must be defined')
+    else:
+        raise Exception('Please specify mode to compute phosphosites')
+
+    # generate all phospho-Isoforms
+    return _build_phosphoSeqome(sequence, _phosphosites, phospho_rate=phospho_rate)
