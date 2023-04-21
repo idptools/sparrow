4feb25812a59ad1ca4a13815b369ea050142bcb4
Jeff Lotthammer <jeffrey.lotthammer@gmail.com>
2023-04-21 14:24:39 -0500

    minor fix to loading torch models

--- sparrow/predictors/batch_predict.py
+++ sparrow/predictors/batch_predict.py
@@ -49,10 +49,11 @@ def prepare_model(network,version,gpuid):
     # Check if GPU is available
     if torch.cuda.is_available():
         device = torch.device(f"cuda:{gpuid}")
+        loaded_model = torch.load(saved_weights,map_location=device)
     else:
         device = torch.device("cpu")
+        loaded_model = torch.load(saved_weights,map_location=device)
     
-    loaded_model = torch.load(saved_weights)
 
     # count number of network layers
     num_layers = 0
