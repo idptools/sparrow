84c0dabb2758352c156dd2788266ebd1c15a0f29
Jeff Lotthammer <jeffrey.lotthammer@gmail.com>
2024-03-22 14:53:19 -0500

    add hydrophobicity scales and edit calc_line_hydrophobicity

--- sparrow/calculate_parameters.py
+++ sparrow/calculate_parameters.py
@@ -122,10 +122,38 @@ def calculate_linear_hydrophobicity(s, mode='KD', normalize=False):
         Selector for hydrophobicity table. Options available are
 
         'KD'    | Kyte-Doolittle
+        "ab"    | Aboderin
+        'al'    | Abraham and Leo
+        'ag'    | Argos
+        'bm'    | Black and Mould
+        'bb'    | Bull and Breese
+        'cs'    | Casari and Sippl
+        'ci'   | Cid
+        'cw3.4' | Cowan3.4
+        'cw7.5' | Cowan7.5
+        'es'    | Eisenberg
+        'eg'    | Engelman
+        'fs'    | Fasman
+        'fc'    | Fauchere
+        'gd'    | Goldsack
+        'guy'   | Guy
+        'jo'    | jones
+        'ju' | Juretic
+        'ki'    | Kidera
+        'mi' |  Miyazawa
+        'pa' | parker
+        'po' | Ponnuswamy
+        'ro' | Rose
+        'rm' | Roseman
+        'sw' | Sweet
+        'ta' | tanford
+        'wi' | wilson
+        'zi' | zimmerman
+
 
     normalize : bool
         Boolean that means hydrophobicity scales operate on a normalized
-        dynamic range of 0 to 1
+        dynamic range of 0 to 1. This only works for Kyle-Doolittle scale.
 
     Returns:
     ------------
@@ -134,14 +162,22 @@ def calculate_linear_hydrophobicity(s, mode='KD', normalize=False):
         a given hydrophobicity scale.
     
     """
-
     if mode == 'KD':
         try:
             if normalize:
-                return [amino_acids.AA_hydro_KD_normalized[r] for r in s]
+                return [amino_acids.HYDROPHOBICITY_SCALES["normalized_kd"][r] for r in s]
             else:
-                return [amino_acids.AA_hydro_KD[r] for r in s]
+                return [amino_acids.HYDROPHOBICITY_SCALES["scaled_kd"][r] for r in s]
         except KeyError:
             raise sparrow_exceptions.CalculationException('Invalid residue found in %s' %(s))
+    elif mode == "cw3.4":
+        cw3_4 = amino_acids.HYDROPHOBICITY_SCALES["cw"][3.4]
+        return [cw3_4[r] for r in s]
+    elif mode == "cw7.5":
+        cw7_5 = amino_acids.HYDROPHOBICITY_SCALES["cw"][7.5]
+        return [cw7_5[r] for r in s]
     else:
-        raise sparrow_exceptions.CalculationException('Invalid mode passed: %s' %(mode))
+        try:
+            return [amino_acids.HYDROPHOBICITY_SCALES[mode][r] for r in s]
+        except KeyError:
+            raise sparrow_exceptions.CalculationException('Invalid mode passed: %s' %(mode))
